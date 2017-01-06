6117a900ac68a28871e8efba538e9187c66a9cca
xieyubai <xieyubai17@gmail.coml>
2017-01-06 23:10:57 +0800

    修改路径

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   root 'jobs#index'
 
   namespace :admin do
-  resources :jobs
-end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+    end
+  end
 end
